f66f1f4470c5d8093f5244f1f675c6a92e6611af
Ryan Balla <ryan@ryanballa.com>
2021-04-04 12:11:12 -0500

    Test

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -86,7 +86,7 @@ const checkAuthUser = async () => {
 
     const authUser = await auth0Client.getUser();
     const token = await auth0Client.getIdTokenClaims();
-
+    console.log(token.id_token);
     // In DEV we load the user from settings
     if (SNOWPACK_PUBLIC_LOGGED_IN_USER_ID) {
         isAuthenticated.set(true);
@@ -105,7 +105,6 @@ const checkAuthUser = async () => {
         clubs.addClubs({ _id: usersReq.clubs[0]._id });
     } else {
         isAuthenticated.subscribe(isLoggedIn => {
-            console.log(token);
             if (isLoggedIn && authUser) {
                 if (!user._id) {
                     setAuthUser(authUser, token);
