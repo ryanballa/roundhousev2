0c6ae8aeebfa7d6adb444f02932e50992875449b
Ryan Balla <ryan@ryanballa.com>
2021-07-10 09:13:22 -0500

    Removing extra delete

--- src/components/elements/TableButtonDelete.svelte
+++ src/components/elements/TableButtonDelete.svelte
@@ -5,11 +5,12 @@
   export let rowOwner = '';
   export let deleteAction = () => {};
   import { user } from '../../store/user';
+
 </script>
 
 <Button
   disabled={$user.isAdmin ? false : row[rowOwner]._id !== $user._id}
   actionText="Delete"
   variant="secondary"
-  actionEvent={() => deleteAction(row._id)}>Delete</Button
->
+  actionEvent={() => deleteAction(row._id)}
+/>
